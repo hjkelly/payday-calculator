26e232aae3b23f452be0a112b8bb3fe94c1364fe
Harrison Kelly <hjkelly@gmail.com>
2019-01-07 07:17:28 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/shopspring/decimal"
+)
+
+// MODELS =====================================================================
+
+type Config struct {
+	Paydays  []Payday  `json:"paydays"`
+	Expenses []Expense `json:"expenses"`
+	Bills    []Bill    `json:"bills"`
+}
+
+type Payday struct {
+	Frequency string          `json:"frequency"`
+	Amount    decimal.Decimal `json:"amount"`
+}
+
+const (
+	SemimonthlyFirstHalf       = "semimonthlyFirstHalf"
+	SemimonthlySecondHalf      = "semimonthlySecondHalf"
+	SemimonthlyFirstHalfStart  = 1
+	SemimonthlySecondHalfStart = 15
+)
+
+type Expense struct {
+	Name    string          `json:"name"`
+	Amount  decimal.Decimal `json:"amount"`
+	Balance decimal.Decimal `json:"balance"`
+}
+
+type Bill struct {
+	Name     string          `json:"name"`
+	Amount   decimal.Decimal `json:"amount"`
+	DueOnDay int             `json:"dueOnDay"`
+}
+
+// MAIN PROCESS ===============================================================
+
+func main() {
+	fmt.Printf("Welcome to the payday calculator!\n")
+
+	// determine config name
+	configFile, err := filepath.Abs("config.json")
+	if err != nil {
+		fmt.Printf("Error while getting absolute path: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// load config
+	config, err := openConfig(configFile)
+	if err != nil {
+		fmt.Printf("Couldn't load config from %s:\n%s\n", configFile, err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Successful config load: %s\n", configFile)
+	// fmt.Printf("%+v\n", config)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	// guess and confirm which half of the month we're doing the budget for
+	halfMonth := guessHalfMonth(time.Now().Day())
+	response := ""
+	if halfMonth == SemimonthlyFirstHalf {
+		fmt.Printf("It seems like you're preparing the budget for the FIRST HALF of the month. Is that right? Type 'y' or 'n'\n")
+		response, _ = reader.ReadString('\n')
+		if strings.HasPrefix(strings.ToLower(strings.TrimSpace(response)), "n") {
+			halfMonth = SemimonthlySecondHalf
+		}
+	} else {
+		fmt.Printf("It seems like you're preparing the budget for the SECOND HALF of the month. Is that right? Type 'y' or 'n'\n")
+		response, _ = reader.ReadString('\n')
+		if strings.HasPrefix(strings.ToLower(strings.TrimSpace(response)), "n") {
+			halfMonth = SemimonthlyFirstHalf
+		}
+	}
+
+	// FINALIZE PREVIOUS ==================================================
+
+	// collect expensetotals for each category
+	// TODO
+
+	// load balances from previous file, if possible
+	// TODO
+
+	// fill up funds / steal from emergency fund for each category
+	// TODO
+
+	// save finalized balances to file
+	// TODO 10
+
+	// PLAN NEXT ==========================================================
+
+	// confirm income
+	income := decimal.NewFromFloat(0)
+	for _, payday := range config.Paydays {
+		income = income.Add(payday.Amount)
+	}
+	fmt.Printf("We expect your total income to put toward this budget is %s.\n", income)
+	fmt.Printf("Provide a different amount if necessary. Press enter to accept.\n")
+	rawIncome, _ := reader.ReadString('\n')
+	rawIncome = strings.TrimSpace(rawIncome)
+	if len(rawIncome) > 0 {
+		parsedIncome, err := decimal.NewFromString(rawIncome)
+		if err != nil {
+			fmt.Printf("Couldn't understand your input. Sorry!\n%s\n", err.Error())
+			os.Exit(2)
+		}
+		income = parsedIncome
+	}
+
+	// calculate giving
+	//totalGiving := decimal.NewFromFloat(0)
+
+	// get half of expenses ----------
+	// loop through expenses, collecting the total amount and getting a table ready for output
+	table := NewTable("EXPENSES", "Name", "Amount")
+	expenseTotal := decimal.NewFromFloat(0)
+	percentage := decimal.NewFromFloat(0.5)
+	for _, expense := range config.Expenses {
+		// half the amount since we're only budgeting for half the month
+		budgetedItemAmount := expense.Amount.Mul(percentage)
+		// increase the total
+		expenseTotal = expenseTotal.Add(budgetedItemAmount)
+		// get it ready for output
+		table.AddRow(expense.Name, budgetedItemAmount)
+	}
+	table.Print()
+
+	// confirm bill amounts
+	// TODO 04
+
+	// total bills for this half ---------
+	// find which bills are relevant
+	bills := make([]Bill, 0)
+	for _, bill := range config.Bills {
+		if halfMonth == SemimonthlyFirstHalf {
+			if bill.DueOnDay >= SemimonthlyFirstHalfStart && bill.DueOnDay < SemimonthlySecondHalfStart {
+				bills = append(bills, bill)
+			}
+		} else {
+			if bill.DueOnDay < SemimonthlyFirstHalfStart || bill.DueOnDay >= SemimonthlySecondHalfStart {
+				bills = append(bills, bill)
+			}
+		}
+	}
+	table = NewTable("BILLS DUE", "Name", "Amount")
+	// loop through expenses, collecting the total amount and getting a table ready for output
+	billTotal := decimal.NewFromFloat(0)
+	for _, bill := range bills {
+		billTotal = billTotal.Add(bill.Amount)
+		table.AddRow(bill.Name, bill.Amount)
+	}
+	table.Print()
+
+	// remind manual bills to pay
+	// TODO 06
+
+	// provide savings leftovers (or what needs to be stolen from emergency fund)
+	balance := income.Sub(expenseTotal).Sub(billTotal)
+	if balance.IsPositive() {
+		fmt.Printf("This much is left over to save: %s\n", balance.String())
+	} else if balance.IsNegative() {
+		fmt.Printf("You don't have enough; can you steal this much from your emergency fund? %s\n", balance.Abs().String())
+	} else {
+		fmt.Printf("Wow, you broke even. Exactly. :|\n")
+	}
+
+	// save results to a file
+	// TODO 05
+
+	os.Exit(0)
+}
+
+// HELPER FUNCTIONS ===========================================================
+
+func openConfig(filepath string) (Config, error) {
+	config := Config{}
+
+	// Open our jsonFile
+	jsonFile, err := os.Open(filepath)
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		return config, err
+	}
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal([]byte(byteValue), &config)
+
+	return config, err
+}
+
+func guessHalfMonth(day int) string {
+	if day >= 10 && day < 20 {
+		return SemimonthlySecondHalf
+	}
+	return SemimonthlyFirstHalf
+}
---
+++ table.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/shopspring/decimal"
+)
+
+type Datum struct {
+	Name   string
+	Amount decimal.Decimal
+}
+
+type Table struct {
+	Caption string
+	Headers []string
+	Data    []Datum
+}
+
+func NewTable(caption string, nameHeader, amountHeader string) *Table {
+	return &Table{
+		Caption: caption,
+		Headers: []string{nameHeader, amountHeader},
+		Data:    []Datum{},
+	}
+}
+
+func (t *Table) AddRow(name string, amount decimal.Decimal) {
+	t.Data = append(t.Data, Datum{name, amount})
+}
+
+func (t *Table) Print() {
+	maxNameLength := 0
+	for _, d := range t.Data {
+		if len(d.Name) > maxNameLength {
+			maxNameLength = len(d.Name)
+		}
+	}
+
+	maxAmountLength := 10
+	maxLength := maxNameLength + 1 + maxAmountLength
+	rowFormat := "%-" + strconv.Itoa(maxNameLength) + "s %" + strconv.Itoa(maxAmountLength) + "s\n"
+
+	fmt.Println("\n")
+	fmt.Printf(t.Caption)
+	captionLength := len(t.Caption)
+	for captionLength < maxLength {
+		fmt.Printf("=")
+		captionLength++
+	}
+	fmt.Println("")
+
+	// headers
+	fmt.Printf(rowFormat, t.Headers[0], t.Headers[1])
+
+	// header separator bar
+	i := 0
+	for i < maxLength {
+		fmt.Printf("-")
+		i++
+	}
+	fmt.Println("")
+
+	// data rows
+	for _, d := range t.Data {
+		fmt.Printf(rowFormat, d.Name, d.Amount.String())
+	}
+
+	// ending bar
+	i = 0
+	for i < maxLength {
+		fmt.Printf("=")
+		i++
+	}
+	fmt.Println("\n")
+}
